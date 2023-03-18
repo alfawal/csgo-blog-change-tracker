0fc4bd0b134798dd23b08867b1f672a3b6158b88
Abdulrahman Alfawal <abdulrahman.hadi4@gmail.com>
2023-03-18 19:21:08 +0300

    Modernize for the end-user

--- csgo_blog_change_tracker.py
+++ csgo_blog_change_tracker.py
@@ -1,27 +1,10 @@
 import logging
 import time
-from collections import defaultdict
 from itertools import count
 from datetime import datetime
 import requests
 import difflib
-
-now_capture = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-logging_format = "%(asctime)s [%(levelname)s]: %(message)s"
-logging_level = logging.INFO
-
-logging.basicConfig(
-    level=logging_level,
-    format=logging_format,
-    datefmt="%Y-%m-%d %H:%M:%S",
-    filename=f"changes-{now_capture}.log",
-)
-
-print_handler = logging.StreamHandler()
-print_handler.setLevel(logging_level)
-print_handler.setFormatter(logging.Formatter(logging_format))
-
-logging.getLogger().addHandler(print_handler)
+import sys
 
 
 CSGO_BLOG_URLS = {
@@ -30,7 +13,30 @@ CSGO_BLOG_URLS = {
 }
 
 
-def main(timeout: int):
+def main(
+    *,
+    timeout: int,
+    print_log: bool,
+    print_log_level: int | str,
+):
+    print("Running CSGO Blog Change Tracker... (Press Ctrl+C to stop)")
+    now_capture = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    logging_format = "%(asctime)s [%(levelname)s]: %(message)s"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format=logging_format,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename=f"changes-{now_capture}.log",
+    )
+
+    if print_log:
+        print_handler = logging.StreamHandler()
+        print_handler.setLevel(print_log_level)
+        print_handler.setFormatter(logging.Formatter(logging_format))
+
+        logging.getLogger().addHandler(print_handler)
+
     session = requests.Session()
     responses = {label: None for label in CSGO_BLOG_URLS}
     check_counter = count(1)
@@ -79,4 +85,32 @@ def main(timeout: int):
 
 
 if __name__ == "__main__":
-    raise SystemExit(main(5))
+    args = sys.argv[1:]
+
+    if "--help" in args or "-h" in args:
+        print(
+            "CSGO Blog Change Tracker by @alfawal\n"
+            "Usage: python csgo_blog_change_tracker.py [options]\n"
+            "Options:\n"
+            "  --timeout <seconds> - The amount of seconds to wait between"
+            " checks. Defaults to 5.\n"
+            "  --print-log - Whether to print the log to the console.\n"
+            "  --print-log-level <level> - The level of the log to print to"
+            " the console (e.g. INFO, ERROR, CRITICAL etc.)."
+            " Defaults to INFO.\n"
+        )
+        raise SystemExit
+
+    try:
+        main(
+            timeout=int(args[args.index("--timeout") + 1])
+            if "--timeout" in args
+            else 5,
+            print_log="--print-log" in args,
+            print_log_level=str(args[args.index("--print-log-level") + 1])
+            if "--print-log-level" in args
+            else logging.INFO,
+        )
+    except KeyboardInterrupt:
+        print("Stopping CSGO Blog Change Tracker...")
+        raise SystemExit
